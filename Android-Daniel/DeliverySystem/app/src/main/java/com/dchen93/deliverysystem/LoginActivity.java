3fb4b4aee2c96c51742a04c8a9bea99b4af08493
Daniel Chen <dnlchen@ucdavis.edu>
2015-05-16 00:44:27 -0700

    added login screen with asynctask, added navigation activity with homefragment

--- Android-Daniel/DeliverySystem/app/src/main/java/com/dchen93/deliverysystem/LoginActivity.java
+++ Android-Daniel/DeliverySystem/app/src/main/java/com/dchen93/deliverysystem/LoginActivity.java
@@ -1,17 +1,54 @@
 package com.dchen93.deliverysystem;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
-public class LoginActivity extends ActionBarActivity {
+public class LoginActivity extends ActionBarActivity implements OnClickListener {
+
+    private EditText user, password;
+    private Button mLogin, mRegister;
+
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // Setup input fields
+        user = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+
+        // Setup buttons
+        mLogin = (Button) findViewById(R.id.login);
+        mRegister = (Button) findViewById(R.id.register);
+        mLogin.setOnClickListener(this);
+        mRegister.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.login:
+                new LoginAttempt().execute();
+                break;
+            case R.id.register:
+                new RegisterAttempt().execute();
+                break;
+            default:
+                break;
+        }
     }
 
     @Override
@@ -34,5 +71,67 @@ public class LoginActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    class LoginAttempt extends AsyncTask<String, String, String> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            //setup progress dialog
+            progressDialog = new ProgressDialog(LoginActivity.this);
+            progressDialog.setMessage(getString(R.string.progress_login));
+            progressDialog.setIndeterminate(true);
+            progressDialog.setCancelable(true);
+            progressDialog.show();
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            /** TO-DO:
+             * communicate with server, parse JSON */
+
+            //if there's a match
+            if(true){
+                Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                finish();
+                LoginActivity.this.startActivity(intent);
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            progressDialog.dismiss();
+            Toast.makeText(getApplicationContext(), R.string.error_login, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    class RegisterAttempt extends AsyncTask<String, String, String> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            //setup progress dialog
+            progressDialog = new ProgressDialog(LoginActivity.this);
+            progressDialog.setMessage(getString(R.string.progress_register));
+            progressDialog.setIndeterminate(true);
+            progressDialog.setCancelable(true);
+            progressDialog.show();
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            /** TO-DO:
+             * communicate with server, parse JSON */
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            progressDialog.dismiss();
+            Toast.makeText(getApplicationContext(), R.string.success_register, Toast.LENGTH_SHORT).show();
+        }
     }
 }
