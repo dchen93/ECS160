3714f980ad150b7ebf712e292d60b1a285a83b75
Daniel Chen <dnlchen@ucdavis.edu>
2015-06-02 13:37:43 -0700

    fixed map activity, added locational awareness

--- Android-Daniel/DeliverySystem/app/src/main/java/com/dchen93/deliverysystem/MapsActivity.java
+++ Android-Daniel/DeliverySystem/app/src/main/java/com/dchen93/deliverysystem/MapsActivity.java
@@ -1,23 +1,67 @@
 package com.dchen93.deliverysystem;
 
+import android.location.Location;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 
-public class MapsActivity extends FragmentActivity {
+public class MapsActivity extends FragmentActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener{
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+    private GoogleApiClient mGoogleApiClient;
+    private Location mLastLocation;
+    private double mLatitude;
+    private double mLongitude;
+    private LatLng currentLocation = null;
+
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
+                mGoogleApiClient);
+        if (mLastLocation != null) {
+            mLatitude = mLastLocation.getLatitude();
+            mLongitude =  mLastLocation.getLongitude();
+
+            currentLocation = new LatLng(mLatitude,mLongitude);
+            if(mMap != null) mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLocation,10));
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        // only need to connect once for now
+        Log.d("MapActivity: ", "ConnectionSuspended");
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.d("MapsActivity: ", "ConnectionFailed");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+        buildGoogleApiClient();
         setUpMapIfNeeded();
+        mMap.setMyLocationEnabled(true);
     }
 
     @Override
@@ -61,9 +105,13 @@ public class MapsActivity extends FragmentActivity {
      * This should only be called once and when we are sure that {@link #mMap} is not null.
      */
     private void setUpMap() {
-        LatLngBounds SOUTH_BAY = new LatLngBounds(new LatLng(37.195331, -122.33139), new LatLng(37.520619, -121.503971));
-        // Set the camera to the greatest possible zoom level that includes the bounds
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(SOUTH_BAY.getCenter(), 10));
+        if(currentLocation != null) {
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLocation,10)); }
+        else {
+            LatLngBounds SOUTH_BAY = new LatLngBounds(new LatLng(37.195331, -122.33139), new LatLng(37.520619, -121.503971));
+            // Set the camera to the greatest possible zoom level that includes the bounds
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(SOUTH_BAY.getCenter(), 10));
+        }
         mMap.setMapType(1);
     }
 }
